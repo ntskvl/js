0e1faa2a45c3d15b0b01f359c56f552b1d55b0f5
ni <102757866+ntskvl@users.noreply.github.com>
2022-05-04 16:45:55 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,16 @@
+console.log('Olá, mundo!');
+console.log(7+4);
+console.log('7'+'4');
+
+console.log(12+20);
+console.log(25+157);
+console.log(30+16);
+
+console.log('Nicolly ' + 'Borges');
+
+const nome = ('Nicolly');
+console.log(nome)
+
+const a = 1200;
+const b = 1500;
+console.log(a+b);
---
+++ exercicios.js
@@ -0,0 +1,28 @@
+// 1-a
+console.log(10+20*30);
+// 1-b
+console.log(4**2/30);
+// 1-c
+console.log((9**4+2)*6-1);
+
+// 2-a 
+console.log(10%3*10**1+1-10*4/2)
+
+// 3
+const nome = ('Nicolly');
+console.log(nome);
+
+// 4
+const x = 3
+const y = 5
+console.log(2*x*3*y);
+
+// 5
+const a = 2
+const b = 4
+const c = 6
+console.log(a+b+c)
+
+// 6
+const d = 1800
+console.log(d*2);
